5c38513e159fed17207fb99a2f1a6996b5a56b7f
Oscar Marriaga <ojmarriagac@zurich.co>
2017-03-09 20:08:46 -0500

    arreglo a url de la araña para allmusic

--- allmusic/allmusic/spiders/allmusic.py
+++ allmusic/allmusic/spiders/allmusic.py
@@ -4,8 +4,8 @@ import scrapy
 
 class AllmusicSpider(scrapy.Spider):
     name = "allmusic"
-    allowed_domains = ["http://www.allmusic.com/genres"]
-    start_urls = ['http://http://www.allmusic.com/genres/']
+    allowed_domains = ["www.allmusic.com"]
+    start_urls = ['http://www.allmusic.com/genres/']
 
     def parse(self, response):
         pass
