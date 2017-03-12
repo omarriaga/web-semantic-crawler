b2f42b3a75d2637e07885b8b1b5c97da09eb4bd6
laurasofiarestrepolondono <1e9e2f8995>
2017-03-12 13:43:59 -0500

    hola

---
+++ wikilyrics/wikilyrics/items.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WikilyricsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class ArtistItem(scrapy.Item):
+    name = scrapy.Field()
+    country = scrapy.Field()
+    state = scrapy.Field()
+    city =scrapy.Field()
+
+
+class SongItem(scrapy.Item):
+    artist = scrapy.Field()
+    name = scrapy.Field()
+    urlYouTube = scrapy.Field()
+    letra = scrapy.Field()
+    album= scrapy.Field()
+    musicBy=scrapy.Field()
+    letraBy=scrapy.Field()
+
+
+class GenreItem(scrapy.Item):
+    name=scrapy.Field()
+    stylisticOrigins=scrapy.Field()
+    instrumentsUsed=scrapy.Field()
+    description=scrapy.Field()
+
+
+class AlbumItem(scrapy.Item):
+    name=scrapy.Field()
+    year=scrapy.Field()
+    length=scrapy.Field()
+    genre=scrapy.Field()
+    urlWikipedia=scrapy.Field()
+    imagen=scrapy.Field()
+
+
+class RecoveryLabelItem(scrapy.Item):
+    name=scrapy.Field()
+    description=scrapy.Field()
+    imagen=scrapy.Field()
+
+
+class ArtistAlbumItem(scrapy.Item):
+    artist=scrapy.Field()
+    album=scrapy.Field()
+
+
+class SongAlbumItem(scrapy.Item):
+    song=scrapy.Field()
+    album=scrapy.Field()
+
+
+class ArtistGenreItem(scrapy.Item):
+    artist=scrapy.Field()
+    genre=scrapy.Field()
+
+
+class RecoveryLabelArtistItem(scrapy.Item):
+    recoveryLabel=scrapy.Field()
+    artist=scrapy.Field()
---
+++ wikilyrics/wikilyrics/pipelines.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy.exporters import CsvItemExporter
+
+
+class ScrapperPipeline(object):
+    @staticmethod
+    def process_item(item, spider):
+        return item
+
+
+def item_type(item):
+    return type(item).__name__.replace('Item', '').lower()  # TeamItem => team
+
+
+CSVDir = '/Users/juan/dumps/'
+
+
+class MultiCSVItemPipeline(object):
+    SaveTypes = ['artist', 'song', 'genre']
+
+    def __init__(self):
+        self.files = dict([(name, open(CSVDir + name + '.csv', 'w+b')) for name in self.SaveTypes])
+        self.exporters = dict([(name, CsvItemExporter(self.files[name], True, ';', delimiter=';')) for name in self.SaveTypes])
+        dispatcher.connect(self.spider_opened, signal=signals.spider_opened)
+        dispatcher.connect(self.spider_closed, signal=signals.spider_closed)
+
+    def spider_opened(self, spider):
+        [e.start_exporting() for e in self.exporters.values()]
+
+    def spider_closed(self, spider):
+        [e.finish_exporting() for e in self.exporters.values()]
+        [f.close() for f in self.files.values()]
+
+    def process_item(self, item, spider):
+        what = item_type(item)
+        if what in set(self.SaveTypes):
+            self.exporters[what].export_item(item)
+        return item
